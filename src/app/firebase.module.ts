7b4105d9a7bf95758f1b422ae0fec4e7586fa767
Carlos Lobo <clobo@bancolombia.com.co>
2024-05-10 09:37:09 -0500

    fix: se remueven credenciales

--- src/app/firebase.module.ts
+++ src/app/firebase.module.ts
@@ -4,19 +4,11 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environments';
 
 @NgModule({
   imports: [
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'paths-storage',
-        appId: '1:509557320033:web:9858722cd9e0c94a087d20',
-        storageBucket: 'paths-storage.appspot.com',
-        apiKey: 'AIzaSyCX2PpTAYi3Yj7kwrBUZwITw3AnhGOAGEk',
-        authDomain: 'paths-storage.firebaseapp.com',
-        messagingSenderId: '509557320033',
-      })
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideFunctions(() => getFunctions()),
